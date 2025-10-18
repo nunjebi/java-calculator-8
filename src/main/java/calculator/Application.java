366fb8ce7a2ecccb47031e7922c0c0df7eb732cf
nunjebi <nunjebi6535@gmail.com>
2025-10-19 02:08:02 +0900

    feat(parser): 문자열에서 쉼표와 콜론을 처리하여 숫자를 추출 후 합을 구하여 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,21 +1,36 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+    public static List<Integer> parseNumbers(String str) {
+        return Arrays.stream(str.split("[,:]"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     public static String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 
-    public static void printAns(String ans) {
+    public static void printAns(int ans) {
         System.out.printf("결과: %s", ans);
     }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         String str = readInput();
-        
-        printAns(str);
+        List<Integer> v = parseNumbers(str);
+
+        int ans = 0;
+        for (int k : v)
+            ans += k;
+
+        printAns(ans);
     }
 }
