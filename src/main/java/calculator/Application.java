88cdb81018ac892427a12a0799d72254f843b560
nunjebi <nunjebi6535@gmail.com>
2025-10-19 00:51:59 +0900

    feat(app): 입출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static String readInput() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void printAns(String ans) {
+        System.out.printf("결과: %s", ans);
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String str = readInput();
+        
+        printAns(str);
     }
 }
